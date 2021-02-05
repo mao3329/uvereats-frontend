f8f9319b441d07211083cf1d83164c1f77e274b4
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-05 16:53:20 +0900

    UpdateFoodsComponents

--- src/containers/Foods.jsx
+++ src/containers/Foods.jsx
@@ -1,12 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+// reducers
+import {
+  initialState as foodsInitialState,
+  foodsActionTyps,
+  foodsReducer,
+} from '../reducers/foods';
+
+// apis
+import { fetchFoods } from '../apis/foods';
+
+// constants
+import { REQUEST_STATE } from '../constants';
 
 export const Foods = (props) => {
+
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
+  useEffect(() => {
+    dispatch({ type: foodsActionTyps.FETCHING });
+    fetchFoods(props.match.params.restaurantsId)
+      .then((data) => {
+        dispatch({
+          type: foodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            foods: data.foods
+          }
+        });
+      });
+  }, [])
+
   return (
     <Fragment>
       フード一覧
-      <p>
-        restaurantsIdは {props.match.params.restaurantsId} です
-      </p>
+      {
+        foodsState.fetchState === REQUEST_STATE.LOADING ?
+          <Fragment>
+            <p>
+              ロード中...
+            </p>
+          </Fragment>
+          :
+          foodsState.foodsList.map(food =>
+            <div key={food.id}>
+              {food.name}
+            </div>
+          )
+      }
     </Fragment>
   )
 }
