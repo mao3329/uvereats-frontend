b76c731e52196d81efd1e8929ab7841809c02f1e
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-02-08 18:48:45 +0900

    Update Orders.jsx

--- src/containers/Orders.jsx
+++ src/containers/Orders.jsx
@@ -1,6 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useReducer } from 'react';
+
+// apis
+import { fetchLineFoods } from '../apis/line_foods';
+import { postOrder, postsOrder } from '../apis/orders';
+
+// reducers
+import {
+  initialState,
+  lineFoodsActionTyps,
+  lineFoodsReducer,
+} from '../reducers/lineFoods';
 
 export const Orders = () => {
+  const [state, dispatch] = useReducer(lineFoodsReducer, initialState);
+
+  const postLineFoods = () => {
+    dispatch({ type: lineFoodsActionTyps.POSTING });
+    postOrder({
+      line_food_ids: state.lineFoodsSummary.line_food_ids,
+    }).then(() => {
+      dispatch({ type: lineFoodsActionTyps.POST_SUCCESS });
+      window.location.reload();
+    });
+  };
+
+  useEffect(() => {
+    dispatch({ type: lineFoodsActionTyps.FETCHING });
+    fetchLineFoods()
+      .then((data) =>
+        dispatch({
+          type: lineFoodsActionTyps.FETCH_SUCCESS,
+          payload: {
+            lineFoodsSummary: data
+          }
+        })
+      )
+      .catch((e) => console.error(e));
+  }, []);
   return (
     <Fragment>
       注文一覧
